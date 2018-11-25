23f089ebfc25310a8cd073feba4a3048f14e328a
uufish <uufish.io@gmail.com>
2018-11-26 00:48:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createGenerateClassName } from '@material-ui/styles'
 import React, { Component } from 'react'
+import JssProvider from 'react-jss/lib/JssProvider'
+import AppRouter from './AppRouter'
+
+const generateClassName = createGenerateClassName({
+  dangerouslyUseGlobalCSS: false,
+  productionPrefix: 'c'
+})
 
 class App extends Component {
   render() {
-    return <Button>Hello World</Button>
+    return (
+      <JssProvider generateClassName={generateClassName}>
+        <>
+          <CssBaseline />
+          <AppRouter />
+        </>
+      </JssProvider>
+    )
   }
 }
 
