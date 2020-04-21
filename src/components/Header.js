11d7baf374aa47100a41b52349cb4354faa60424
reiwa <reiwa@outlook.com.vn>
2020-04-22 01:40:02 +0700

    refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <AppBar position={'sticky'} color={'default'}>
+    <AppBar color={'default'} position={'sticky'}>
       <Toolbar>
         <Typography color={'inherit'} component={Link} to={'/'} variant={'h5'}>
           {'Forum'}
